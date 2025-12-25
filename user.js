263fa10f247f0342a818a8232e99fd0d6725048e
franksmc2008-create <franksmc2008@gmail.com>
2025-12-25 23:13:36 +0300

    Update user.js

--- user.js
+++ user.js
@@ -1,15 +1,13 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
+    fullName: { type: String, required: true },
     phone: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     balance: { type: Number, default: 0 },
-    // This is the code for their unique referral link
-    referralCode: { type: String, unique: true }, 
-    // This links them to the person who invited them
-    referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
-    // Becomes true once they pay the 150/- registration fee
-    isPaid: { type: Boolean, default: false }
+    isActivated: { type: Boolean, default: false }, // Becomes true after M-Pesa payment
+    referralCode: { type: String, unique: true, default: () => Math.random().toString(36).substring(2, 8) },
+    dateJoined: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
